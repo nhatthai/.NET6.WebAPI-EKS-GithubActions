7226326fdd0bdfa505c96adee726a5131c46c4a3
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2022-10-26 14:48:45 +0700

    create service account and role

--- devops/terraform/lb-controller.tf
+++ devops/terraform/lb-controller.tf
@@ -17,11 +17,30 @@ module "lb_irsa_role" {
   }
 }
 
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+        "app.kubernetes.io/name"= "aws-load-balancer-controller"
+        "app.kubernetes.io/component"= "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${local.account_id}:role/aws-load-balancer-controller"
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
+
 resource "helm_release" "alb" {
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   name  	=  "aws-load-balancer-controller"
   namespace = "kube-system"
+
+  depends_on = [
+    kubernetes_service_account.service-account
+  ]
 
   set {
     name  = "region"
--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -1,6 +1,11 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    command     = "aws"
+  }
 }
 
 provider "aws" {
@@ -20,5 +25,11 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      command     = "aws"
+    }
   }
 }
