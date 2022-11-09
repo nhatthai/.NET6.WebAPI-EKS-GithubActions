d4f0df58f8c257b48484ef245cf6c5a914d81d5f
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2022-11-09 16:12:18 +0700

    Remove Unused files

--- devops/AWS/aws_iam_policy-lb2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_policy" "load-balancer-policy" {
-  name        = "AWSLoadBalancerControllerIAMPolicy"
-  path        = "/"
-  description = "AWS LoadBalancer Controller IAM Policy"
-
-  policy = file("iam-policy.json")
-
-}
